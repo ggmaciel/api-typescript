e6e51ee6a72a159e3692a59a7ad902703a8e2d1c
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-08-08 10:37:05 -0300

    desacoplar lógica de requests do axios  e errors

--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,5 +1,7 @@
-import { AxiosStatic } from 'axios'
+import * as HTTPUtil from '@src/util/request'
 import { InternalError } from '@src/util/errors/internal-error'
+import config, { IConfig } from 'config'
+
 
 export interface StormGlassPointSource {
     [key: string]: number
@@ -46,23 +48,25 @@ export class StormGlassResponseError extends InternalError {
     }
 }
 
+const stormGlassResourceConfig: IConfig = config.get('App.resources.StormGlass')
+
 export class StormGlass {
     readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed'
     readonly stormGlassAPISource = 'noaa'
-    constructor(protected request: AxiosStatic) { }
+    constructor(protected request = new HTTPUtil.Request()) { }
 
     public async fetchPoints(lat: number, lng: number): Promise<ForecastPoint[]> {
         try {
-            const response = await this.request.get<StormGlassForecastResponse>(`https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
+            const response = await this.request.get<StormGlassForecastResponse>(`${stormGlassResourceConfig.get('apiUrl')}/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&end=1592113802&lat=${lat}&lng=${lng}`,
                 {
                     headers: {
-                        Authorization: 'fake-token'
+                        Authorization: stormGlassResourceConfig.get('apiToken')
                     }
                 }
             )
             return this.normalizeResponse(response.data)
         } catch (err) {
-            if (err.response && err.response.status) {
+            if (HTTPUtil.Request.isRequestError(err)) {
                 throw new StormGlassResponseError(`Error: ${JSON.stringify(err.response.data)} Code: ${err.response.status}`)
             }
             throw new ClientRequestError(err.message)
