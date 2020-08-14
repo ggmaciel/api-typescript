4671bd88df37b0bea037f8da3f438d89805e2699
Gustavo Maciel <gu.ga.14@hotmail.com>
2020-08-14 14:29:40 -0300

    functional test for beach module

--- src/database.ts
+++ src/database.ts
@@ -4,7 +4,7 @@ import mongoose, { Mongoose } from 'mongoose';
 const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<Mongoose> =>
-    await mongoose.connect(dbConfig.get('mongoUrl'), {
+    await mongoose.connect(dbConfig.get('mongoURL'), {
         useCreateIndex: true,
         useNewUrlParser: true,
         useUnifiedTopology: true,
